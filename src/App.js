444084b37ce7237a30f207ea6ca66bad50fde428
PhutawanNick <phuthawan7417@gmail.com>
2025-05-10 20:00:02 +0700

    edit navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LivePage from './pages/live/LivePage';
 import DashBoard from './pages/dashboard/DashBoardPage';
@@ -6,20 +7,21 @@ import Settings from './pages/setting/SettingPage';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 
-
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Sidebar />
-        <Routes>
-          <Route path="/" element={<DashBoard />} />
-          <Route path="/LivePage" element={<LivePage />} />
-          <Route path="/DashBoard" element={<DashBoard />} />
-          <Route path="/History" element={<History />} />
-          <Route path="/Setting" element={<Settings />} />
-        </Routes>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<DashBoard />} />
+            <Route path="/LivePage" element={<LivePage />} />
+            <Route path="/DashBoard" element={<DashBoard />} />
+            <Route path="/History" element={<History />} />
+            <Route path="/Setting" element={<Settings />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
