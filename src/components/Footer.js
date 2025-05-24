798eb937061c293b26efd73ac56d53f3e877faa4
PhutawanNick <phuthawan7417@gmail.com>
2025-05-25 02:17:13 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,29 +5,11 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-section">
-          <h4>About Us</h4>
-          <p>Your company description here</p>
+        <div className="footer-text">
+          <span className='left-text'>2025 All Right Reserved</span>
+          <span className="center-text">Project Helmet & Rider Detection System By KMUTT</span>
+          <span className="right-text">Logo</span>
         </div>
-        
-        <div className="footer-section">
-          <h4>Contact</h4>
-          <p>Email: contact@example.com</p>
-          <p>Phone: (123) 456-7890</p>
-        </div>
-
-        <div className="footer-section">
-          <h4>Follow Us</h4>
-          <div className="social-links">
-            <a href="#">Facebook</a>
-            <a href="#">Twitter</a>
-            <a href="#">Instagram</a>
-          </div>
-        </div>
-      </div>
-      
-      <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
       </div>
     </footer>
   );
