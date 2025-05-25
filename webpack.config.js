900444952334beec2a8f5ccafcc149c3513b275a
Phutawan <phuthawan.cha@gmail.com>
2025-05-26 02:36:38 +0700

    add .env file to disable source map generation; create webpack.config.js for JavaScript source map handling

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // ...existing config
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+        exclude: /node_modules\/react-datepicker/
+      }
+    ]
+  }
+};
